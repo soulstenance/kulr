b332fab4f503306a4fdf10b9f8387bb762126663
soulstenance <soulstenance.e2pis@slmail.me>
2025-05-16 19:11:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## kulr
+### A simple Python color library using ANSI escape sequences to spruce up your Python scripts with ease
+I'm just messing around and learning Python. While there are surely better options for color formatting, feel free to give this a try and leave any feedback you have!
+
+#### License Notice
+This library is licensed under MIT and is provided as-is with no guarantee of suitability for a particular purpose. Use at your own risk. A copy of the license should accompany this library so you can always know your rights and responsibilities.
---
+++ kulr.py
@@ -0,0 +1,61 @@
+# Thanks for using my color library.
+# This library is licensed under MIT and is provided as-is with no guarantee of suitability for a particular purpose. Use at your own risk.
+
+# Reset formatting
+
+reset = "\033[0m"
+
+# Standard text colors
+
+black = "\033[30m"
+red = "\033[31m"
+green = "\033[32m"
+yellow = "\033[33m"
+blue = "\033[34m"
+magenta = "\033[35m"
+cyan = "\033[36m"
+white = "\033[37m"
+
+# Bright text colors
+
+bblack = "\033[90m"
+bred = "\033[91m"
+bgreen = "\033[92m"
+byellow = "\033[93m"
+bblue = "\033[94m"
+bmagenta = "\033[95m"
+bcyan = "\033[96m"
+bwhite = "\033[97m"
+
+# Background colors
+
+black_bg = "\033[40m"
+red_bg = "\033[41m"
+green_bg = "\033[42m"
+yellow_bg = "\033[43m"
+blue_bg = "\033[44m"
+magenta_bg = "\033[45m"
+cyan_bg = "\033[46m"
+white_bg = "\033[47m"
+
+# Bright background colors
+
+bblack_bg = "\033[100m"
+bred_bg = "\033[101m"
+bgreen_bg = "\033[102m"
+byellow_bg = "\033[103m"
+bblue_bg = "\033[104m"
+bmagenta_bg = "\033[105m"
+bcyan_bg = "\033[106m"
+bwhite_bg = "\033[107m"
+
+# Other formatting
+
+bold = "\033[1m"
+test = "\033[2m" # This makes the text greyer I guess?
+italic = "\033[3m"
+under = "\033[4m"
+blink = "\033[5m"
+test2 = "\033[6m" # Also blinking text? Idk what's going on...
+reverse = "\033[7m" # Seems to make a white background but documentation says "Reverse video on...
+conceal = "\033[8m" # Hides text but it can still be copied out and pasted
